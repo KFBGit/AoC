f9682b78482e10ec9be29a29255b19fc43d3e1f0
LarsSchmidgall <lars@schmidgall.org>
2022-12-03 23:54:53 +0100

    Add to for loop to sort data into 2d array

--- 01/code/whoToEat.py
+++ 01/code/whoToEat.py
@@ -2,7 +2,15 @@
 import sys
 
 filename=sys.argv[1]
+counterOfElfs=0
+elfCalories=[]
+elfCalories.append([])
 
 with open(filename, 'r') as file:
     for line in file:
-        print(line.rstrip())
+        if line == '\n':
+            counterOfElfs=counterOfElfs + 1
+            elfCalories.append([])
+            continue
+        
+        elfCalories[counterOfElfs].append(int(line.rstrip()))
