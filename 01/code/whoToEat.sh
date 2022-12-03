501ab425293fc05f3756f9309009654aa0cf07a6
LarsSchmidgall <lars@schmidgall.org>
2022-12-03 04:21:48 +0100

    calculate total but unable to leave for

--- 01/code/whoToEat.sh
+++ 01/code/whoToEat.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-i=1
+calcTotalCalsPerElf() {
+	local totalCaloriesPerElf=() caloriesOfElf=0
 
-for line in ../input/calories.input
-do
-	if $line==""
-	then
-		i=$((i+1))
-	else
-		echo ""
-	fi
-done
+	cat $1 | while read line
+	do
+	#	echo "working on: $line"
+		[[ $line == "" ]] && totalCaloriesPerElf+=("$caloriesOfElf") && caloriesOfElf=0 && continue
+		echo "${totalCaloriesPerElf[@]}"
+	#	echo "adding line to tempvar: $line"
+		((caloriesOfElf+=line))
+	#	echo $caloriesOfElf
+	#	if [[ $caloriesOfElf == 54911 ]]; then
+	#		echo "found the culprit..."
+	#	fi
+
+	done
+	totalCaloriesPerElf+=("$caloriesOfElf")
+	echo "${totalCaloriesPerElf[@]}"
+}
+
+calcTotalCalsPerElf "$1"
